ec1d2ba19873b5731f8dffaa080d68ff9fa4f376
damon <drdamanraaj@gmail.com>
2020-07-07 15:31:51 +0000

    New file added, with (finally) some code too.

---
+++ example.py
@@ -0,0 +1,10 @@
+"""
+This module prints a message about the new file added to the repositry.
+"""
+def git_opeation():
+"""
+Prints a message.
+"""
+ print("I am adding example.py file to the remote repository.")
+git_opeation()
+
